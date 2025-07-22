660a4d9f45332cff02abc92c2e09c65f90f915ec
Romain-dev85 <r.auvinet@gmail.com>
2025-07-22 19:25:00 +0200

    end of CP 4

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -11,4 +11,5 @@ VALUES
 ('PinkFloyd', 'The Dark Side of the Moon (1973)', 'https://media.senscritique.com/media/000004795486/0/the_dark_side_of_the_moon.jpg'),
 ('The Beatles', 'Abbey Road (1969)', 'https://media.senscritique.com/media/000006055988/0/abbey_road.jpg'),
 ('The Velvet Underground & Nico (1967)', 'The Velvet Underground & Nico', 'https://media.senscritique.com/media/000004846519/0/the_velvet_underground_nico.jpg'),
-('The Clash', 'London Calling (1979)', 'https://media.senscritique.com/media/000004821700/0/london_calling.jpg');
+('The Clash', 'London Calling (1979)', 'https://media.senscritique.com/media/000004821700/0/london_calling.jpg'),
+('Miles Davis', 'Bitches Brew (1970)', 'https://media.senscritique.com/media/000006048145/0/bitches_brew.jpg');
